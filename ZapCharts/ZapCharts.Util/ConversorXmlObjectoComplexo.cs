4de883d993583a477f585b2bcc0cba914ba037c4
Altamir Dias Cassiano <altamirdiascassiano@gmail.com>
2016-10-24 00:46:40 -0200

    Primeiro Commit

---
+++ ZapCharts/ZapCharts.Util/ConversorXmlObjectoComplexo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using ZapCharts.Model.Transiente.Zap2._5._0;
+
+namespace ZapCharts.Util
+{
+    public class ConversorXmlObjectoComplexo
+    {
+        public static ZapReportXml Converter(string CaminhoXml)
+        {
+            var zapReportXml = new ZapReportXml();
+            XmlDataDocument xmldoc = new XmlDataDocument();
+            XmlNodeList xmlnode;
+            FileStream fs = new FileStream(CaminhoXml, FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+
+            xmlnode = xmldoc.GetElementsByTagName("site");
+            zapReportXml.NomeSite = xmlnode[0].Attributes["host"].Value;
+
+            xmlnode = xmldoc.GetElementsByTagName("alertitem");
+            for (int i = 0; i <= xmlnode.Count - 1; i++)
+            {
+                var descricaoAlerta = xmlnode[i].ChildNodes.Item(1).InnerText.Trim();
+                var qtdeOcorrencias = xmlnode[i].ChildNodes.Item(8).InnerText.Trim();
+         
+                zapReportXml.Alertas.Add(new Alerta()
+                {
+                    NomeAlerta = descricaoAlerta,
+                    QtdOcorrencias = Convert.ToInt32(qtdeOcorrencias)
+                });
+            }
+            fs.Close();
+            return zapReportXml;
+        }
+    }
+}
---
+++ ZapCharts/ZapCharts/Scripts/Helper/MensagemHelper.js
@@ -0,0 +1,24 @@
+﻿function MostrarMensagem(tipo, mensagem) {
+    var css= ""; 
+    switch(tipo.toLowerCase()) {
+        case 'sucesso':
+            css = "alert alert-success";
+            break;
+        case 'atencao':
+            css = "alert alert-warning";
+            break;
+        case 'informacao':
+            css = "alert alert-info";
+            break;
+        case 'erro':
+            css = "alert alert-danger";
+            break;
+        default:
+            css = "alert alert-info";
+    }
+    $("#caixaMensagem").removeClass();
+    $("#caixaMensagem").addClass(css);
+    $("#caixaMensagem").fadeIn(1000, function () { });
+    $("#mensagem").html(mensagem);
+    $("#caixaMensagem").fadeOut(10000, function () {});
+}
